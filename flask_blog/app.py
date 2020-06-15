e83560516038261729b454924e962f125be1019c
Dmitry Ganin <dg@bamboogroup.eu>
2020-06-15 23:48:30 +0300

    open post on new page

--- flask_blog/app.py
+++ flask_blog/app.py
@@ -1,6 +1,7 @@
 import sqlite3
 
 from flask import Flask, render_template
+from werkzeug.exceptions import abort
 
 app = Flask(__name__)
 
@@ -13,7 +14,22 @@ def index():
     return render_template('index.html', posts=posts)
 
 
+@app.route('/<int:post_id>')
+def post(post_id):
+    p = get_post(post_id)
+    return render_template('post.html', post=p)
+
+
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
+
+
+def get_post(post_id):
+    conn = get_db_connection()
+    p = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    conn.close()
+    if p is None:
+        abort(404)
+    return p
